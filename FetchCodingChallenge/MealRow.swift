45c6353be80db5c5a95faba65c1be5a7bca1c234
terrencejihoonjung <terrencejung928@gmail.com>
2024-01-04 01:46:55 -0800

    working on UI

---
+++ FetchCodingChallenge/MealRow.swift
@@ -0,0 +1,18 @@
+//
+//  MealRow.swift
+//  FetchCodingChallenge
+//
+//  Created by Terrence Jung on 1/4/24.
+//
+
+import SwiftUI
+
+struct MealRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MealRow()
+}
