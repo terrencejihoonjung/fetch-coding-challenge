70040b16dafab06784e101d0f402d0e5a24caeea
terrencejihoonjung <terrencejung928@gmail.com>
2024-01-04 00:43:25 -0800

    Added API service

---
+++ FetchCodingChallenge/API/APIService.swift
@@ -0,0 +1,8 @@
+//
+//  APIService.swift
+//  FetchCodingChallenge
+//
+//  Created by Terrence Jung on 1/4/24.
+//
+
+import Foundation
---
+++ FetchCodingChallenge/API/Endpoint.swift
@@ -0,0 +1,8 @@
+//
+//  Endpoint.swift
+//  FetchCodingChallenge
+//
+//  Created by Terrence Jung on 1/3/24.
+//
+
+import Foundation
